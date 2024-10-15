91fc9f0dde3ea6c4caf99bec0f8a5f49a30fc0e9
Ricardo de Sousa <ricardo.desousa96@gmail.com>
2024-10-15 18:20:44 +0200

    Fixed deploy setup

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',       // Enables static export
+    assetPrefix: './',      // Ensures relative paths for assets like CSS, JS
+};
 
 export default nextConfig;
