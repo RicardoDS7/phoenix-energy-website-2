0000145244739c977901fb70c6df32105ced8909
Ricardo de Sousa <ricardo.desousa96@gmail.com>
2024-10-15 21:14:05 +0200

    new

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
         <meta name="description" content={metadata.description} />
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="/logo.png" />
+        <link rel="icon" href={`/logo.png`} />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet" />
