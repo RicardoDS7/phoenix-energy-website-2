7d52604c60aae32db7205423bf9cc19e6a9b1d92
Ricardo de Sousa <ricardo.desousa96@gmail.com>
2024-10-15 20:11:08 +0200

    temp image disable

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
     output: 'export',       // Enables static export
     assetPrefix: './',      // Ensures relative paths for assets like CSS, JS
+    images: {
+        unoptimized: true,  // Disable Next.js Image Optimization just for GitHub
+      },
 };
 
 export default nextConfig;
