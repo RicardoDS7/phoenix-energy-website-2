3ea979b7205c8cc099cabc2f2034c86bbf2ce3bf
Ricardo de Sousa <ricardo.desousa96@gmail.com>
2024-10-15 23:05:33 +0200

    Update config

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -10,6 +10,15 @@ const nextConfig = {
   },
   assetPrefix: isGithubPages ? '/phoenix-energy-website-2/' : '',  // GitHub Pages vs. local/other deployment
   basePath: isGithubPages ? '/phoenix-energy-website-2' : '',  // Conditionally set basePath
+
+  webpack: (config) => {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.NEXT_PUBLIC_BASE_PATH': JSON.stringify(isGithubPages ? '/phoenix-energy-website-2' : ''),
+      })
+    );
+    return config;
+  },
 };
 
 export default nextConfig;
