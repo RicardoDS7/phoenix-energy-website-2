1869e1c0fd7ed8b0f626604247f940bbae1f25ce
RicardoDS7 <ricardo.desousa96@gmail.com>
2024-11-18 11:02:51 +0200

    Add sitemap

---
+++ website/next-sitemap.js
@@ -0,0 +1,6 @@
+module.exports = {
+    siteUrl: 'https://phoenixenergy.tech', // Your website's URL
+    generateRobotsTxt: true, // Generate robots.txt as well
+    outDir: './public', // Generate the sitemap in the public directory
+  };
+  
